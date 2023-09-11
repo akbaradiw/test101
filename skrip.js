8623ea1103873afa5a9befe34ffb0a05d0e7b23a
akbaradiw <akbaradiw000@gmail.com>
2023-09-11 17:44:21 +0700

    first commit

---
+++ skrip.js
@@ -0,0 +1,39 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello, world!";
+
+// alert("wow");
+
+// belajar variable
+// var namaDepan = "akbar",
+//   umur = 31,
+//   lulus = Alhamdulillah;
+// console.log(namaDepan);
+
+// var angka =
+
+// tips while
+// var ulang = true;
+// while (ulang) {
+//   console.log("hello world");
+//   ulang = confirm("lagi");
+// }
+
+// output hello word lopping 10x
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 10) {
+//   console.log("hello world");
+//   nilaiAwal++;
+// }
+
+// output hello word muncul di console log 48x
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 48) {
+//   console.log("hello world " + nilaiAwal + "x");
+//   nilaiAwal++;
+// }
+
+var noAngkot = 1;
+while (noAngkot <= 10) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  noAngkot++;
+}
